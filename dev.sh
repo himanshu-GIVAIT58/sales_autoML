9d18908c48181bbd943a14e51c644e2aab7ca2cb
himanshu-GIVAIT58 <himanshu.yadav@giva.co>
2025-07-20 13:33:05 +0000

    update

--- dev.sh
+++ dev.sh
@@ -2,6 +2,9 @@
 echo "Starting development environment with hot reload in detached mode..."
 
 # Start Docker Compose with hot reload and detached mode
+sudo docker compose down
+
 sudo docker compose build 
 
 sudo docker compose up -d
+
